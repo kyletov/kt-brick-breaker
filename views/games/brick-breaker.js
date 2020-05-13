532e2e98f63b202c8ffb8a8f0241416b290cdb26
Kyle Wing-Hoong Tov <kyle.tov@mail.utoronto.ca>
2020-05-13 02:28:14 -0400

    Fixing error

--- views/games/brick-breaker.js
+++ views/games/brick-breaker.js
@@ -1,38 +1,38 @@
 <script type="text/babel">
-	class GameCanvas extends React.Component {
-		constructor(props) {
-			super(props);
-		}
-		render(props){
-			return (
-				<canvas id="stage" width="800" height="800" style={{border:"1px solid black"}} >
-				</canvas>
-			);
-		}
-	}
+	// class GameCanvas extends React.Component {
+	// 	constructor(props) {
+	// 		super(props);
+	// 	}
+	// 	render(props){
+	// 		return (
+	// 			<canvas id="stage" width="800" height="800" style={{border:"1px solid black"}} >
+	// 			</canvas>
+	// 		);
+	// 	}
+	// }
 
-	class ViewComponent extends React.Component {
-		constructor(props) {
-			super(props);
-			this.state = {
-				"currentState": true,
-			}
-			this.clickHandler = this.clickHandler.bind(this);
-		}
+	// class ViewComponent extends React.Component {
+	// 	constructor(props) {
+	// 		super(props);
+	// 		this.state = {
+	// 			"currentState": true,
+	// 		}
+	// 		this.clickHandler = this.clickHandler.bind(this);
+	// 	}
 
-		clickHandler(e) {
-			
-		}
+	// 	clickHandler(e) {
 
-		render() {
-			return (
-				<h1>Welcome to KT Brick Breaker!</h1>
-				<GameCanvas />
-			);
-		}
-	}
+	// 	}
 
-	ReactDOM.render( <ViewComponent />,
+	// 	render() {
+	// 		return (
+	// 			<h1>Welcome to KT Brick Breaker!</h1>
+	// 			<GameCanvas />
+	// 		);
+	// 	}
+	// }
+
+	ReactDOM.render( <h1>Hello World!</h1>,
     document.getElementById('root')
   );
 </script>
